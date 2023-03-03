99eec291387d4e3ff11a002737706872e2247846
KaanIsmet <kaanismet7@gmail.com>
2023-03-03 16:29:55 -0500

    added activate function

--- main.c
+++ main.c
@@ -4,14 +4,27 @@
 #include <netinet/in.h>
 #include <gtk/gtk.h>
 
+static void
+activate (GtkApplication* app,
+          gpointer        user_data)
+{
+  GtkWidget *window;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_widget_show_all (window);
+}
+
 int main(int argc, char *argv[]) {
-    GtkWidget *window;
+	GtkApplication *app;	
+	int status;
+	
+	app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+	
+	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+	status = g_application_run (G_APPLICATION (app), argc, argv);
+	g_object_unref (app);
 
-    gtk_init(&argc, &argv);
-
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_widget_show(window);
-
-    gtk_main();
-    return 0;
+	return status;
 }
